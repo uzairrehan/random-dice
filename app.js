95c0b79b13dbb85a11bb7d05a8b98f56191a5eaf
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-09-21 18:58:49 +0500

    added dices

--- app.js
+++ app.js
@@ -1,25 +1,68 @@
-// all ok
-let pic = document.querySelectorAll("img");
+let main = document.querySelectorAll(".main");
+let button = document.querySelector("#button");
 function ghumao() {
   let random = Math.ceil(Math.random() * 6);
   switch (random) {
     case 1:
-      pic[0].src = "Images/1.png";
+      main[0].innerHTML = `      <div class="dice1 dice">
+        <div class="round d1"></div>
+      </div>` ;
       break;
     case 2:
-      pic[0].src = "Images/2.png";
+      main[0].innerHTML = `      <div class="dice2 dice">
+        <div class="round d1"></div>
+        <div class="round d2"></div>
+      </div>` ;
       break;
     case 3:
-      pic[0].src = "Images/3.png";
+      main[0].innerHTML = `      <div class="dice3 dice">
+        <div class="round d1"></div>
+        <div class="round d2"></div>
+        <div class="round d3"></div>
+      </div>`;
       break;
     case 4:
-      pic[0].src = "Images/4.png";
+      main[0].innerHTML = `      <div class="dice4 dice">
+        <div class="first-line">
+          <div class="round "></div>
+          <div class="round "></div>
+        </div>
+        <div class="last-line">
+          <div class="round "></div>
+          <div class="round "></div>
+        </div>
+      </div>` ;
       break;
     case 5:
-      pic[0].src = "Images/5.png";
+      main[0].innerHTML = `      <div class="dice5 dice">
+        <div class="first-line">
+            <div class="round "></div>
+            <div class="round "></div>
+          </div>
+          <div class="middle">
+            <div class="round"></div>
+          </div>
+          <div class="last-line">
+            <div class="round "></div>
+            <div class="round "></div>
+            
+      </div>
+      </div>` ;
       break;
     case 6:
-      pic[0].src = "Images/6.png";
+      main[0].innerHTML = `        <div class="dice6 dice">
+          <div class="first-line">
+              <div class="round "></div>
+              <div class="round "></div>
+              <div class="round "></div>
+          </div>
+          <div class="last-line">
+              <div class="round "></div>
+              <div class="round "></div>
+              <div class="round "></div>
+          </div>
+        </div>`;
       break;
   }
 }
+button.addEventListener("click", ghumao() )
